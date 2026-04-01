e01efcce5c5986dcb1da15322aa5f910a1d9af6d
Arcandis <akshadha.bikumalla@gmail.com>
2026-04-01 21:16:53 +0530

    fixed website email servies

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ app.config['MAIL_PORT'] = int(os.environ.get('MAIL_PORT', 587))
 app.config['MAIL_USE_TLS'] = os.environ.get('MAIL_USE_TLS', True)
 app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME', '')
 app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD', '')
-app.config['MAIL_DEFAULT_SENDER'] = os.environ.get('MAIL_DEFAULT_SENDER', 'noreply@lensandluxe.com')
+app.config['MAIL_DEFAULT_SENDER'] = ('Lens&Luxe', 'noreply@lensandluxe.com')
 
 db = SQLAlchemy(app)
 mail = Mail(app)
