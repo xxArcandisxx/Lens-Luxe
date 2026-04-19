901472ab4970793394dafdee920191f50f9b0508
Arcandis <akshadha.bikumalla@gmail.com>
2026-04-19 14:46:26 +0530

    removed unnecessary files

--- db_migration.py
+++
@@ -1,7 +0,0 @@
-from app import app, db
-from sqlalchemy import text
-
-with app.app_context():
-    db.session.execute(text('ALTER TABLE "user" ADD COLUMN IF NOT EXISTS bio VARCHAR(500) DEFAULT \'\';'))
-    db.session.commit()
-    print('Database altered successfully!')
